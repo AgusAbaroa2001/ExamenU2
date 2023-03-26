7056eb420c913489d2521a7f1128b4c3e65b8c89
Agustin Abaroa <124708562+AgusAbaroa2001@users.noreply.github.com>
2023-03-26 13:31:36 -0600

    Splash independiente

--- src/Splash.java
+++ src/Splash.java
@@ -1,12 +1,74 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Splash extends JFrame {
-	
-	public Splash() {
-		System.out.println("Hola a todos");
-	}
-	
+    private static final long serialVersionUID = 1L;
+    private JLabel label;
+    private JPanel panel;
+
+    public Splash() {
+        super("Splash Screen");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setResizable(false);
+
+        // Centrar la ventana en la pantalla
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width/2-getSize().width/2, dim.height/2-getSize().height/2);
+
+        // Crear el panel con la imagen
+        panel = new JPanel() {
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            public void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Image image = new ImageIcon("img\splash.jpg").getImage();
+                g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+
+        // Agregar un borde al panel
+        panel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+
+        // Agregar una etiqueta con el mensaje de carga
+        label = new JLabel("Cargando...");
+        label.setFont(new Font("Arial", Font.BOLD, 20));
+        label.setForeground(Color.WHITE);
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        // Agregar la etiqueta al panel
+        panel.add(label);
+
+        // Agregar el panel a la ventana
+        add(panel, BorderLayout.CENTER);
+
+        // Hacer visible la ventana
+        setVisible(true);
+
+        // Agregar una pausa de tiempo antes de cerrar el Splash Screen
+        try {
+            Thread.sleep(3000); // Pausa durante 3 segundos (3000 milisegundos)
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Cerrar la ventana del Splash Screen
+        dispose();
+    }
+
+    public static void main(String[] args) {
+        new Splash();
+    }
 }
